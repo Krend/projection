25b6056899d76e645ba259d496dd11bf1ff93191
Krend <zoli.barkoczi@gmail.com>
2025-04-05 18:22:36 +0200

    Refactor Point3D attributes to lowercase and update matrix operation functions for consistency; add new utility functions for distance calculations and matrix approximations.

--- my_math_functions.py
+++ my_math_functions.py
@@ -1,7 +1,9 @@
 from typing import List
 from dataclasses import dataclass
+import random
+import math
 # This file contains various mathematical functions used in the project.
-# It includes functions for matrix.m operations, 3D to 2D projection, and 2D to 3D projection.
+# It includes functions for matrix operations, 3D to 2D projection, and 2D to 3D projection.
 # The functions are designed to work with 3D points and matrices, and they handle various transformations.
 
 #type definitions
@@ -12,9 +14,9 @@ class Point2D:
 
 @dataclass
 class Point3D:
-    X: float
-    Y: float
-    Z: float
+    x: float
+    y: float
+    z: float
 
 @dataclass
 class Matrix3x3:
@@ -42,7 +44,7 @@ class Rect:
         self.pt0 = Point2D(0, 0)
         self.pt1 = Point2D(0, 0)
 
-#Utility functions for matrix.m operations
+#Utility functions for matrix operations
 
 # matrix.m size of 9
 def adjugate3x3(matrix: Matrix3x3):
@@ -240,19 +242,19 @@ def source_to_dest(x_source: int, y_source: int, combined_matrix: Matrix3x3):
 # Return the rectange which is around the quad
 # quad.pts size of 4
 def quad_to_rect(quad: Quad): 
-    if len(quad) != 4:
+    if len(quad.pts) != 4:
         return -1
     rect = Rect()
-    rect.pt0.X = quad.pts[0].X
-    rect.pt1.X = quad.pts[0].X
-    rect.pt0.Y = quad.pts[0].Y
-    rect.pt1.Y = quad.pts[0].Y
+    rect.pt0.x = quad.pts[0].x
+    rect.pt1.x = quad.pts[0].x
+    rect.pt0.y = quad.pts[0].y
+    rect.pt1.y = quad.pts[0].y
 
     for i in range(1, 3):
-        rect.pt0.X = min(rect.pt0.X, quad.pts[i].X)
-        rect.pt1.X = max(rect.pt1.X, quad.pts[i].X)
-        rect.pt0.Y = min(rect.pt0.Y, quad.pts[i].Y)
-        rect.pt1.Y = max(rect.pt1.Y, quad.pts[i].Y)
+        rect.pt0.x = min(rect.pt0.x, quad.pts[i].x)
+        rect.pt1.x = max(rect.pt1.x, quad.pts[i].x)
+        rect.pt0.y = min(rect.pt0.y, quad.pts[i].y)
+        rect.pt1.y = max(rect.pt1.y, quad.pts[i].y)
 
     return rect
 
@@ -261,9 +263,10 @@ def quad_to_rect(quad: Quad):
 # The function returns the projected 2D coordinates of the point.
 # matrix.m size of 16
 def project_3d_2d(pt3d: Point3D, width: int, height: int, matrix: Matrix4x4):
-    x = matrix.m[0]  * pt3d.X + matrix.m[1]  * pt3d.Y + matrix.m[2]  * pt3d.Z + matrix.m[3]
-    y = matrix.m[4]  * pt3d.X + matrix.m[5]  * pt3d.Y + matrix.m[6]  * pt3d.Z + matrix.m[7]
-    w = matrix.m[12] * pt3d.X + matrix.m[13] * pt3d.Y + matrix.m[14] * pt3d.Z + matrix.m[15]
+    x = matrix.m[0]  * pt3d.x + matrix.m[1]  * pt3d.y + matrix.m[2]  * pt3d.z + matrix.m[3]
+    y = matrix.m[4]  * pt3d.x + matrix.m[5]  * pt3d.y + matrix.m[6]  * pt3d.z + matrix.m[7]
+    # z axis is not used in 2D projection
+    w = matrix.m[12] * pt3d.x + matrix.m[13] * pt3d.y + matrix.m[14] * pt3d.z + matrix.m[15]
     
     if w == 0:
         return -1, -1   #todo: check if this can be a valid result
@@ -277,7 +280,7 @@ def project_3d_2d(pt3d: Point3D, width: int, height: int, matrix: Matrix4x4):
 def project_2d_3d(pt2d: Point2D, width: int, height: int, inv_matrix: Matrix4x4):
     x = (pt2d.x / width) * 2.0 -1.0    
     y = (pt2d.y / height) * 2.0 -1.0
-    z = 0   # No cordinate for z, since we are using a 2D point
+    z = 0   # No value for z, since we are using a 2D point (I'll leave the rest of the code as is for clarity)
 
     x2 = inv_matrix.m[0]  * x + inv_matrix.m[1]  * y + inv_matrix.m[2]  * z + inv_matrix.m[3]
     y2 = inv_matrix.m[4]  * x + inv_matrix.m[5]  * y + inv_matrix.m[6]  * z + inv_matrix.m[7]
@@ -288,4 +291,88 @@ def project_2d_3d(pt2d: Point2D, width: int, height: int, inv_matrix: Matrix4x4)
         return -1, -1, -1   #todo: check if this can be a valid result
     
     return (x2 / w), (y2 / w), (z2 / w)
+
+def point_dist_2d(pt1: Point2D, pt2: Point2D):
+    return ((pt1.x - pt2.x) ** 2 + (pt1.y - pt2.y) ** 2) ** 0.5
+
+def eval_dist(matrix: Matrix4x4, pt3d_list : List[Point3D], pt2d_list: List[Point2D], width: int, height: int):
+    if len(pt3d_list) != len(pt2d_list):
+        return -1
+    
+    dist = 0
+    for i in range(len(pt3d_list)):
+        pt2d = Point2D()
+        pt2d.x, pt2d.y = project_3d_2d(pt3d_list[i], width, height, matrix)
+        dist += point_dist_2d(pt2d, pt2d_list[i])
+
+    return dist
+
+def perturb_matrix(matrix: Matrix4x4, perturbation: float):
+    for i in range(len(matrix.m)):
+        matrix.m[i] += random.uniform(-perturbation, perturbation)
+    return matrix
+
+def approximate_matrix(matrix: Matrix4x4, pt3d_list : List[Point3D], pt2d_list: List[Point2D], width: int, height: int):
+    if len(pt3d_list) != len(pt2d_list):
+        return -1
+    
+    best_matrix = matrix
+    best_dist = eval_dist(matrix, pt3d_list, pt2d_list, width, height)
+    
+    for i in range(1000):  # Number of iterations
+        perturbed_matrix = perturb_matrix(matrix, 0.1)  # Perturbation value
+        dist = eval_dist(perturbed_matrix, pt3d_list, pt2d_list, width, height)
+        
+        if dist < best_dist:
+            best_dist = dist
+            best_matrix = perturbed_matrix
+
+    return best_matrix
+
+def compute_transfer_matrix(pt2d_list: List[Point2D], width: int, height: int):
+    matrix = Matrix4x4()
+    # Initialize the matrix with identity values 
+    # We will distort this until we get the best fit for the points
+    matrix.m[0] = 1.0
+    matrix.m[5] = 1.0
+    matrix.m[10] = 1.0
+    matrix.m[15] = 1.0
+
+    #todo: check if it's required to swap [2] and [3] in pt2d_list (will depend on my input data in this implementation)
+
+    pt3d_list =  List[Point3D]()
+    pt3d_list.append(Point3D(0, 0, 0))
+    pt3d_list.append(Point3D(0, 1, 0))
+    pt3d_list.append(Point3D(1, 0, 0))
+    pt3d_list.append(Point3D(1, 1, 0))
+
+    # Compute the transfer matrix using the approximate_matrix function
+    best_matrix = approximate_matrix(matrix, pt3d_list, pt2d_list, width, height)
+
+    return best_matrix
+
+
+def line_reverse_project_2d_3d(pt2d_list: List[Point2D], width: int, height: int, inv_matrix: Matrix4x4):
+    pt3d_list = []
+    for pt2d in pt2d_list:
+        pt3d = Point3D()
+        pt3d.x, pt3d.y, pt3d.z = project_2d_3d(pt2d, width, height, inv_matrix)
+        pt3d_list.append(pt3d)
+
+    return pt3d_list
+
+def calc_rot_angle(pt_center: Point2D, pt_act: Point2D, pt_top: Point2D):
+    # Calculate the angle between two points with respect to a center point
+    dx1 = pt_act.x - pt_center.x
+    dy1 = pt_act.y - pt_center.y
+    dx2 = pt_top.x - pt_center.x
+    dy2 = pt_top.y - pt_center.y
+
+    #todo: check if this equals 180 - ArcCos((v1.X*v2.X+v1.Y*v2.Y) / (Sqrt(Sqr(v1.X)+Sqr(v1.Y))*Sqrt(Sqr(v2.X)+Sqr(v2.Y)) ))* 180 /pi
+
+    angle1 = math.atan2(dy1, dx1)
+    angle2 = math.atan2(dy2, dx2)
+
+    return angle2 - angle1
+
 
